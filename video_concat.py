75f91480e367df482af128566d9d9acfd21ab3c7
lilei <code.lilei@gmail.com>
2019-05-09 15:28:27 +0800

    change video codec config

--- video_concat.py
+++ video_concat.py
@@ -8,20 +8,22 @@ def concat_video(file1, file2, horizontal=True):
     cap2 = cv2.VideoCapture(file2)
     if not cap1.isOpened() or not cap2.isOpened():
         print('error opening video')
-    w = int(cap1.get(3))
-    h = int(cap1.get(4))
+    # frm_cnt = int(cap1.get(cv2.CAP_PROP_FRAME_COUNT))
+    w = int(cap1.get(cv2.CAP_PROP_FRAME_WIDTH))
+    h = int(cap1.get(cv2.CAP_PROP_FRAME_HEIGHT))
     if horizontal:
         w <<= 1
         axis = 1
     else:
         h <<= 1
         axis = 0
-    out = cv2.VideoWriter("concat.mp4", cv2.VideoWriter_fourcc('F','M','P', '4'), 25, (w, h))
+    # out = cv2.VideoWriter("concat.mp4", cv2.VideoWriter_fourcc(*'MP4V'), 25, (w, h))
+    out = cv2.VideoWriter("concat.avi", cv2.VideoWriter_fourcc(*'XVID'), 25, (w, h))
 
     while(True):
-        ret, frame1 = cap1.read()
-        ret, frame2 = cap2.read()
-        if frame1 is None or frame2 is None:
+        ret1, frame1 = cap1.read()
+        ret2, frame2 = cap2.read()
+        if not ret1 or not ret2:
             break
         frame_con = np.concatenate((frame1, frame2), axis=axis)
         cv2.imshow('frame_con', frame_con)
@@ -36,4 +38,4 @@ def concat_video(file1, file2, horizontal=True):
 
 
 if __name__ == '__main__':
-    concat_video("test1.avi", "test2.avi", True)
+    concat_video("test1.avi", "test2.avi", horizontal=True)
