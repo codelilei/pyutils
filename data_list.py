f5fb52c6abb1ac8544bc8415f73f9e1c75e681a8
lilei <code.lilei@gmail.com>
2019-10-24 14:46:36 +0800

    add split_pos_neg, gen_list

--- data_list.py
+++ data_list.py
@@ -63,7 +63,7 @@ def shuffle_datalist(file):
     list2file(datalist, shufflefile)
 
 
-def proc_datalist(file, train_ratio=None, insert='', append='', label_func=None):
+def proc_datalist(file, train_ratio=None, insert='', append='', label_func=None, pick_num=None):
     datalist = []
     with open(file, encoding='ansi') as fd:
         for line in fd:
@@ -79,6 +79,8 @@ def proc_datalist(file, train_ratio=None, insert='', append='', label_func=None)
             line_new = '{}{} {}{}'.format(insert, imname, label, append)
             datalist.append(line_new)
     random.shuffle(datalist)
+    if pick_num:
+        datalist = datalist[:pick_num]
     root, ext = os.path.splitext(file)
     if train_ratio:
         file_train = root + '_train' + ext
@@ -116,6 +118,52 @@ def filter_line(file, keep_word=None, exclude_word=None, ignore_case=False):
             fd1.write(line + '\n')
 
 
+def split_pos_neg(list_path):
+    path_pre = os.path.splitext(list_path)[0]
+    # fdp = open('{}/{}_pos_neg.txt'.format(path_pre), 'w')
+    fdp = open('{}_pos.txt'.format(path_pre), 'w')
+    fdn = open('{}_neg.txt'.format(path_pre), 'w')
+    for line in open(list_path):
+        tmp = line.strip()
+        # dir_name = tmp.split('/')[1]
+        # dir_name_new = dir_name.replace(' ', '')
+        # line_new = line.replace(dir_name, dir_name_new)
+        # fdpn.write(line_new)
+        if tmp.split()[-1] == '1':
+            fdp.write(line)
+        else:
+            fdn.write(line)
+    # fdpn.close()
+    fdp.close()
+    fdn.close()
+
+
+def gen_list(root_folder, exts=None, keep_root=False, lamda_dir_level=None):
+    root_folder = os.path.realpath(root_folder)
+    parent_folder = os.path.abspath(os.path.join(root_folder, '..'))
+    rt_name = os.path.basename(root_folder)
+    fd = open('{}/{}_list.txt'.format(parent_folder, rt_name), 'w')
+    for rt, d, f in os.walk(root_folder):
+        if len(f) < 0:
+            continue
+        rel_path = os.path.relpath(rt, root_folder)
+        dir_level = 0 if len(rt) == len(root_folder) else len(rel_path.split(os.sep))
+        # print(rt, d, f, dir_level)
+        if lamda_dir_level and not lamda_dir_level(dir_level):
+            continue
+        for fname in f:
+            ext = os.path.splitext(fname)[-1]
+            if not exts or (len(ext) > 0 and ext in exts):
+                if dir_level == 0:
+                    rel_path_new = fname
+                else:
+                    rel_path_new = rel_path.replace('\\', '/') + '/' + fname
+                if keep_root:
+                    rel_path_new = rt_name + '/' + rel_path_new
+                fd.write(rel_path_new + '\n')
+    fd.close()
+
+
 def label_func100(x):
     return str(round(float(x)*100))
 
@@ -135,3 +183,6 @@ def extract_label(file, insert='', append=''):
 
 if __name__ == '__main__':
     proc_datalist('AFLW_LFW-score.txt', 0.8, 'AFLW_LFW-norm/', label_func=label_func100)
+    # split_pos_neg('D:/train_shuffle.txt')
+    # gen_list('test_folder', '.jpg', False, lambda d: d > 0)
+    # gen_list('E:/cellphone_imgs', ['.jpg', '.png'], keep_root=True)
